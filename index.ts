a6069599e3bc01e03eb8796e99459d01a5024575
GauriWeb-Dev <gauriagrawal2201@gmail.com>
2023-10-26 23:53:49 +0530

    feat : initial structure

---
+++ index.ts
@@ -0,0 +1,46 @@
+//import serverless from "serverless-http";
+import express from "express";
+const app = express();
+//import connectDatabase from "./src/database/db";
+import cors from "cors";
+import * as dotenv from "dotenv";
+//import router from "./src/routes/routes";
+//import errorHandler from "./src/middleware/globalErrorHandler";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.use(
+	cors({
+		exposedHeaders: ["etk"],
+	})
+);
+
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+
+// app.use("/", async (req, res, next) => {
+// 	await connectDatabase().catch((error) => {
+// 		next(error);
+// 	});
+// 	next();
+// });
+
+// app.use(router);
+
+app.use((req, res, next) => {
+	console.log("in app start");
+	return res.status(404).json({
+		error: "Not Found",
+	});
+});
+
+// app.use(errorHandler);
+
+app.listen(port,()=>{
+  console.log(`app is running over port : ${port}`)
+})
+
+// module.exports.handler = serverless(app);
